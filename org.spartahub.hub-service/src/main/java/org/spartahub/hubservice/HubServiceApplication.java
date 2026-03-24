ea9aa53abaa2cb90184f671e80d3c7fc8e3e9d55
yonggyo1981 <yonggyo00@kakao.com>
2026-03-24 18:05:25 +0900

    .

---
+++ org.spartahub.hub-service/src/main/java/org/spartahub/hubservice/HubServiceApplication.java
@@ -0,0 +1,13 @@
+package org.spartahub.hubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubServiceApplication.class, args);
+	}
+
+}
---
+++ org.spartahub.hub-service/src/test/java/org/spartahub/hubservice/HubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spartahub.hubservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
