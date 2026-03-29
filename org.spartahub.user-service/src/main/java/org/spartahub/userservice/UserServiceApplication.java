eb7b400a3ad23449cb8f2fa162b3f2b00ae6a198
yonggyo1981 <yonggyo00@kakao.com>
2026-03-30 08:35:03 +0900

    .

--- org.spartahub.user-service/src/main/java/org/spartahub/userservice/UserServiceApplication.java
+++ org.spartahub.user-service/src/main/java/org/spartahub/userservice/UserServiceApplication.java
@@ -1,11 +1,16 @@
 package org.spartahub.userservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.spartahub.common.exception.GlobalExceptionAdviceImpl;
+import org.spartahub.config.security.LoginFilter;
+import org.spartahub.config.security.SecurityConfigImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootApplication
+@Import({SecurityConfigImpl.class, LoginFilter.class, GlobalExceptionAdviceImpl.class})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
