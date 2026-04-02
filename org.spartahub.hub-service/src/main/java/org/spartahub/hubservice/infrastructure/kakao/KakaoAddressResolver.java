6d45f24a227c8517a8e5d5dc027ed535d25c0b24
yonggyo1981 <yonggyo00@kakao.com>
2026-04-02 22:42:06 +0900

    .

--- org.spartahub.hub-service/src/main/java/org/spartahub/hubservice/infrastructure/kakao/KakaoAddressResolver.java
+++ org.spartahub.hub-service/src/main/java/org/spartahub/hubservice/infrastructure/kakao/KakaoAddressResolver.java
@@ -1,11 +1,64 @@
 package org.spartahub.hubservice.infrastructure.kakao;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+import org.spartahub.common.exception.BadRequestException;
+import org.spartahub.common.exception.InternalServerException;
 import org.spartahub.hubservice.domain.hub.service.AddressResolver;
 import org.spartahub.hubservice.domain.hub.service.Coordinates;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestClient;
 
+@Slf4j
+@Component
 public class KakaoAddressResolver implements AddressResolver {
+    @Value("${KAKAO_API_KEY}")
+    private String apiKey;
+
+    private final RestClient restClient = RestClient.builder()
+            .baseUrl("https://dapi.kakao.com")
+            .build();
+
+
     @Override
     public Coordinates resolve(String address) {
-        return null;
+        if (!StringUtils.hasText(address)) {
+            throw new BadRequestException("조회할 주소가 비어있습니다.");
+        }
+
+        try {
+            JsonNode body = restClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/v2/local/search/address.json")
+                            .queryParam("query", address)
+                            .build())
+                    .header("Authorization", "KakaoAK " + apiKey)
+                    .retrieve()
+                    // 4xx, 5xx 에러 발생 시 자동으로 예외를 던지도록 설정 가능
+                    .onStatus(HttpStatusCode::isError, (request, response) -> {
+                        log.error("Kakao API Error: {}", response.getStatusCode());
+                        throw new InternalServerException("외부 API 호출 실패");
+                    })
+                    .body(JsonNode.class);
+
+            if (body != null) {
+                JsonNode documents = body.get("documents");
+                if (documents != null && !documents.isEmpty()) {
+                    JsonNode firstDoc = documents.get(0);
+                    double lon = firstDoc.get("x").asDouble();
+                    double lat = firstDoc.get("y").asDouble();
+
+                    log.info("Address: {} -> Coordinates: {}, {}", address, lat, lon);
+                    return new Coordinates(lat, lon);
+                }
+            }
+        } catch (Exception e) {
+            log.error("주소 변환 중 예외 발생: [Address: {}] Error: {}", address, e.getMessage(), e);
+        }
+
+        throw new BadRequestException("유효하지 않은 주소입니다: " + address);
     }
 }
